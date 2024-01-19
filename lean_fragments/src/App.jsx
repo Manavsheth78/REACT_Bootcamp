51a11fa1f45840f297e95d8ed636978ae6921885
manavsheth.78 <shethmanav2002@gmail.com>
2024-01-19 18:22:24 +0530

    container learning

--- lean_fragments/src/App.jsx
+++ lean_fragments/src/App.jsx
@@ -1,5 +1,6 @@
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -8,11 +9,11 @@ function App() {
   let foodItems = ["Dal", "Rice", "Roti", "Milk", "Green Vegetable"];
 
   return (
-    <>
+    <Container>
       <h1 className="heading">Healthy Food</h1>
       <ErrorMessage items={foodItems} />
       <FoodItems items={foodItems} />
-    </>
+    </Container>
   );
 }
 
