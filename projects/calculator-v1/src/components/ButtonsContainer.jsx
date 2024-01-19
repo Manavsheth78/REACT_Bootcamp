c4468fbbc8612ca9add32a8a8adb2b51820f5d02
manavsheth.78 <shethmanav2002@gmail.com>
2024-01-19 16:06:04 +0530

    custom btns for calculator

--- projects/calculator-v1/src/components/ButtonsContainer.jsx
+++ projects/calculator-v1/src/components/ButtonsContainer.jsx
@@ -1,13 +1,29 @@
 import styles from "./ButtonsContainer.module.css";
 const ButtonsContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "0",
+    ".",
+  ];
+
   return (
     <div className={styles.buttons_container}>
-      <button className={styles.button}>C</button>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>+</button>
-      <button className={styles.button}>3</button>
-      <button className={styles.button}>3</button>
+      {buttonNames.map((btn) => (
+        <button className={styles.button}>{btn}</button>
+      ))}
     </div>
   );
 };
