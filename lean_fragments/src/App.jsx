37bd1d11baeab4f3b39dd9d3f56ff4149d74fea9
manavsheth.78 <shethmanav2002@gmail.com>
2024-01-20 12:03:02 +0530

    complete container

--- lean_fragments/src/App.jsx
+++ lean_fragments/src/App.jsx
@@ -9,11 +9,19 @@ function App() {
   let foodItems = ["Dal", "Rice", "Roti", "Milk", "Green Vegetable"];
 
   return (
-    <Container>
-      <h1 className="heading">Healthy Food</h1>
-      <ErrorMessage items={foodItems} />
-      <FoodItems items={foodItems} />
-    </Container>
+    <>
+      <Container>
+        <h1 className="heading">Healthy Food</h1>
+        <ErrorMessage items={foodItems} />
+        <FoodItems items={foodItems} />
+      </Container>
+
+      <Container>
+        <p>
+          The above list shows the best food for healthy life and well being{" "}
+        </p>
+      </Container>
+    </>
   );
 }
 
