ce3c545a157e2c8580402dd4f34bb2ad33ff4561
manavsheth.78 <shethmanav2002@gmail.com>
2024-01-21 15:28:43 +0530

    learning the useState hook

--- lean_fragments/src/App.jsx
+++ lean_fragments/src/App.jsx
@@ -3,24 +3,36 @@ import ErrorMessage from "./components/ErrorMessage";
 import Container from "./components/Container";
 import FoodInput from "./components/FoodInput";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   // let foodItems = [];
-  let foodItems = ["Dal", "Rice", "Roti", "Milk", "Green Vegetable"];
-  let textToShow = "Food Items Entered By User";
+  // let foodItems = ["Dal", "Rice", "Roti", "Milk", "Green Vegetable"];
 
-  const handleOnChange = (event) => {
-    console.log(event.target.value);
-    textToShow = event.target.value; // this will not work as react will not re-render the component
+  // let textStateArr = useState("Food Items Entered By User");
+  // let textToShow = textStateArr[0]; //initail value of the state
+  // let setTextState = textStateArr[1]; //method
+
+  // let [textToShow, setTextState] = useState(); //destructuring
+
+  let [foodItems, setFoodItems] = useState([]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      //? the props are immutable so we ideally should not change the props so we store it in the new array and then we change it
+      let newFoodItem = event.target.value;
+      event.target.value = "";
+      let newItemList = [...foodItems, newFoodItem];
+      setFoodItems(newItemList);
+    }
   };
 
   return (
     <>
       <Container>
         <h1 className="heading">Healthy Food</h1>
-        <FoodInput handleOnChange={handleOnChange}></FoodInput>
-        <p>{textToShow}</p>
+        <FoodInput handleOnKeyDown={handleKeyDown}></FoodInput>
         <ErrorMessage items={foodItems} />
         <FoodItems items={foodItems} />
       </Container>
--- lean_fragments/src/components/FoodInput.jsx
+++ lean_fragments/src/components/FoodInput.jsx
@@ -1,13 +1,13 @@
 import styles from "./FoodInput.module.css";
 // const FoodInput = (props) => {   //! basically we pass the props from parent to child component and then we use it in the child component but to simply we direclty destructuring the props in the parameter itself as the name handleOnChange and then we use it in the child component
-const FoodInput = ({ handleOnChange }) => {
+const FoodInput = ({ handleOnKeyDown }) => {
   return (
     <input
       type="text"
       className={styles.foodInput}
       placeholder=" Enter Custom Order "
       // onChange={props.handleOnChange}
-      onChange={handleOnChange}
+      onKeyDown={handleOnKeyDown}
     />
   );
 };
